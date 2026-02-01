5e33467cf18639451aee6a313b541a067edc43a6
keeekeeey <snowballintexas@gmail.com>
2026-02-01 17:53:56 -0600

    feat: color scheme and filler images

--- app/componets/draggable.tsx
+++ app/componets/draggable.tsx
@@ -8,10 +8,17 @@ export default function App( {onClose}: {onClose: () => void}) {
 
   return (
     <Draggable nodeRef={nodeRef}>
-      <div className="absolute border min-h-60 min-w-60" 
+      <div className="absolute border min-h-60 min-w-60 bg-foreground-01" 
       ref={nodeRef}>
-        <button onClick={onClose}>Close</button>
-        Drag me!
+        <div className="basis-1/4 border rounded p-4 bg-foreground-02 grid grid-cols-2 gap-4"> 
+        <div></div>
+            <button onClick={onClose}
+            className="bg-foreground-03 rounded p-2 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-accent">
+                <img 
+                className='max-h-5 max-w-5'
+                src="https://cdn-icons-png.flaticon.com/128/10490/10490302.png"></img>
+            </button>
+      </div>
         </div>
     </Draggable>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -11,30 +11,74 @@ export default function Home() {
 
   return (
     <div className="w-full h-full">
-      <div className="basis-1/4 border"> 
+      <div className="basis-1/4 border rounded p-4 bg-foreground-02 grid grid-cols-2 gap-4"> 
+        <div></div>
         <button 
-        onClick={() => {setIsVisible(true)}}
-        className="bg-blue-500 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 ...">
-          Save Changes
+        className="bg-foreground-03 rounded p-2 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-accent">
+          X
         </button>
         {isVisible && (<App  onClose={() => setIsVisible(false)}></App>)}
       </div>
-          <div className="basis-2/4 border">
-              <div className="border min-h-120">
+          <div className="basis-2/4 border rounded p-2 bg-foreground-01">
+              <div className="p-4 border min-h-120">
                 page
-                  <div className="grid grid-cols-3 gap-4 border min-h-20">
-                    <div>grid 1</div>
-                    <div>grid 2</div>
-                    <div>grid 3</div>
-                  </div>
-                  <div className="min-h-10 border"> spacer</div>
                   <div className="grid grid-cols-2 gap-4 border min-h-20">
-                    <div>grid 1</div>
-                    <div>grid 2</div>
+                    <div className="p-1">
+                      <p className="bg-foreground-02 p-1">If you're looking for a problem solver, look no further!</p>
+                      words
+                      </div>
+                    <div>
+                      <img
+                      src="https://i.pinimg.com/736x/a6/70/62/a67062073c4089ef2b624fa3d411251e.jpg">
+                      </img>
+                    </div>
+                  </div>
+                  <div className="min-h-10 border bg-[url(https://i.pinimg.com/1200x/04/c4/99/04c499e22cdf26db8a48f17372b04bb8.jpg)]">
+                  </div>
+                  <div className="grid grid-cols-2 gap-4 border min-h-20 p-2">
+                    <div>
+                        <table className="border-collapse border auto-scroll ">
+                          <thead>
+                          </thead>
+                          <tbody>
+                            <tr>
+                              <td className="border p-2">project name</td>
+                              <td className="border p-2">links</td>
+                            </tr>
+                            <tr>
+                              <td className="border p-2">project name</td>
+                              <td className="border p-2">links</td>
+                            </tr>
+                            <tr>
+                              <td className="border p-2">project name</td>
+                              <td className="border p-2">links</td>
+                            </tr>
+                                                        <tr>
+                              <td className="border p-2">project name</td>
+                              <td className="border p-2">links</td>
+                            </tr>
+                          </tbody>
+                        </table>
+                    </div>
+                    <div className="flex items-center gap-2" >
+                      <button 
+                        className="bg-foreground-02 rounded p-2 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-accent">
+                          <img className="max-h-10 max-w-10" src="https://cdn-icons-png.flaticon.com/256/25/25231.png"></img>
+                      </button>
+                      <button 
+                        className="bg-foreground-02 rounded p-2 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-accent">
+                          <img className="mx-h-10 max-w-10" src="https://cdn-icons-png.flaticon.com/128/3536/3536569.png"></img>
+                      </button>
+                      <button 
+                        onClick={() => {setIsVisible(true)}}
+                        className="bg-foreground-03 rounded p-2 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-accent">
+                          Resume
+                      </button>
+                    </div>
                   </div>
               </div>
           </div>
-        <div className="basis-1/4 border">page 03</div>
+        <div className="basis-1/4 border bg-foreground-02">page 03</div>
         
     </div>
     
