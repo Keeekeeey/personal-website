b7c5e9a2c1585ab083a59d70d6ea5eabd741db07
keeekeeey <snowballintexas@gmail.com>
2026-02-01 22:48:29 -0600

    feat: added basic table page

---
+++ app/pages/project-table.tsx
@@ -0,0 +1,35 @@
+export default function ProjectTable() {
+
+    return (
+         <div className="overflow-y-auto overflow-x-auto max-h-96">
+            <table className="border-collapse min-w-full">
+            <thead></thead>
+                <tbody>
+                    <tr>
+                        <td className=" p-2">project name</td>
+                        <td className=" p-2">links</td>
+                    </tr>
+                    <tr>
+                        <td className=" p-2">project name</td>
+                        <td className=" p-2">links</td>
+                    </tr>
+                    <tr>
+                        <td className=" p-2">project name</td>
+                        <td className=" p-2">links</td>
+                    </tr>
+                    <tr>
+                        <td className=" p-2">project name</td>
+                        <td className=" p-2">links</td>
+                    </tr>
+                    <tr>
+                        <td className=" p-2">project name</td>
+                        <td className=" p-2">   
+                    </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+
+
+} 
